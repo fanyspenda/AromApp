e5d46d628b3385efd69719488ecd374859579239
M Nindra Zaka <mnindrazaka@gmail.com>
2019-11-07 07:48:43 +0700

    remove unused code

--- src/App.js
+++ src/App.js
@@ -5,28 +5,6 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import CreateRecipe from "./pages/CreateRecipe";
 import ListRecipe from "./pages/ListRecipe";
 
-// class Formik extends React.Component {
-//   state = {
-//     values: {
-//       email: "",
-//       password: ""
-//     },
-//     loading: false
-//   };
-
-//   handleChange = (name, value) => {
-//     this.setState();
-//   };
-
-//   render() {
-//     <div>
-//       <h1>Judulnya</h1>
-//       {/* {this.props.children(this.state.values)} */}
-//       {this.props.render({ state: this.state, handleChange: handleChange })}
-//     </div>;
-//   }
-// }
-
 export default class App extends React.Component {
   state = {
     activeItem: "AromApp"
@@ -42,24 +20,6 @@ export default class App extends React.Component {
     return (
       <>
         <Router>
-          {/* <Formik>
-          {({ state, handleChange }) => (
-            <form>
-              <input type="email" value={values.email}></input>
-              <input type="password" value={values.password}></input>
-            </form>
-          )}
-        </Formik>
-
-        <Formik
-          render={values => (
-            <>
-              <input type="email" value={values.email}></input>
-              <input type="password" value={values.password}></input>
-            </>
-          )}
-        /> */}
-
           <Menu color="red" inverted>
             <Menu.Item
               header
